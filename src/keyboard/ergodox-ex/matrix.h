95f7fe4efe48930e964769405e2f879a0f8d911a
Adam Coleman <adam@drunkserver.com>
2018-03-07 17:37:42 -0600

    Move stuff again

--- src/keyboard/ergodox-ex/matrix.h
+++ src/keyboard/ergodox-ex/matrix.h
@@ -56,9 +56,9 @@
                 k37,k38,k39,k3A,k3B,k3C,k3D,         \
                     k28,k29,k2A,k2B,k2C,k2D,         \
                         k19,k1A,k1B,k1C,k1D,         \
-            k08,k0A,k27,                             \
+            k08,k07,k27,                             \
             k0D,k09,k17,k18,                         \
-            k07,k0C,k0B )                            \
+            k0A,k0C,k0B )                            \
                                                      \
     /* matrix positions */                           \
     {{ k00,k01,k02,k03,k04,k05,k06, k07,k08,k09,k0A,k0B,k0C,k0D },  \
